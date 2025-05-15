7ab41775aa5f1ab19da0733474dbc265d8aead47
Edik Krmakov <krmakov2001@mail.ru>
2025-05-15 19:41:37 +0300

    Fix DeveloperInfo

--- MavenPlugin/src/main/java/org/plugin/DeveloperInfo.java
+++ MavenPlugin/src/main/java/org/plugin/DeveloperInfo.java
@@ -1,25 +1,26 @@
 package org.plugin;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.model.Developer;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 import java.util.List;
 
 
-
 @Mojo(name = "developer-info")
 public class DeveloperInfo extends AbstractMojo {
-    @Component
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
     private MavenProject project;
 
     public void execute() throws MojoExecutionException {
         List<Developer> developers = project.getDevelopers();
 
-        if (developers.isEmpty()) {
+        if (CollectionUtils.isEmpty(developers)) {
             getLog().error("No developers found in the project.");
             return;
         }
@@ -27,15 +28,20 @@ public class DeveloperInfo extends AbstractMojo {
         getLog().info("Project Developers Information:");
         for (Developer developer : developers) {
             getLog().info("-----------------------------------");
-            getLog().info("ID: " + developer.getId());
-            getLog().info("Name: " + developer.getName());
-            getLog().info("Email: " + developer.getEmail());
-            getLog().info("URL: " + developer.getUrl());
-            getLog().info("Organization: " + developer.getOrganization());
-            getLog().info("Organization URL: " + developer.getOrganizationUrl());
-            getLog().info("Roles: " + String.join(", ", developer.getRoles()));
-            getLog().info("Timezone: " + developer.getTimezone());
-            getLog().info("Properties: " + developer.getProperties());
+            getLog().info("ID: " + StringUtils.defaultIfEmpty(developer.getId(), "-"));
+            getLog().info("Name: " + StringUtils.defaultIfEmpty(developer.getName(), "-"));
+            getLog().info("Email: " + StringUtils.defaultIfEmpty(developer.getEmail(), "-"));
+            getLog().info("URL: " + StringUtils.defaultIfEmpty(developer.getUrl(), "-"));
+            getLog().info("Organization: " + StringUtils.defaultIfEmpty(developer.getOrganization(), "-"));
+            getLog().info("Organization URL: " + StringUtils.defaultIfEmpty(developer.getOrganizationUrl(), "-"));
+
+            List<String> roles = developer.getRoles();
+            String rolesInfo = (roles != null && !roles.isEmpty())
+                    ? String.join(", ", roles)
+                    : "-";
+            getLog().info("Roles: " + rolesInfo);
+            getLog().info("TimeZone: " + StringUtils.defaultIfEmpty(developer.getTimezone(), "-"));
+            getLog().info("Properties: " + StringUtils.defaultIfEmpty(developer.getProperties().toString(), "-"));
         }
     }
 }
